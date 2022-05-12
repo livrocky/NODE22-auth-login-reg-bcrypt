8255ed05beb3401241f88357510e61486663dd1f
rokas <rokasra@gmail.com>
2022-05-12 14:45:12 +0300

    works

--- src/server.js
+++ src/server.js
@@ -1,19 +1,11 @@
-// server
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const Joi = require('joi');
 const { PORT } = require('./config');
 const { showBody } = require('./middleware');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-
-// users db
-const users = [
-  { email: 'james@bond.com', password: '123456' },
-  { email: 'jane@bdoe.com', password: '123456' },
-];
 
 // MiddleWare
 app.use(morgan('dev'));
@@ -28,13 +20,7 @@ app.get('/', (req, res) => {
 // Routes
 app.use('/', userRoutes);
 
-// GET /users - grazina visus vartotojus json formatu
-app.get('/users', async (req, res) => {
-  res.json(users);
-});
-
 // 404 turetu grazinti json objekta
-
 app.all('*', (req, res) => {
   res.status(404).json({ error: 'Page not found' });
 });
